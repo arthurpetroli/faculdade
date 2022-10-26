d80666b08260c1cd080ffe3780cd27fb7f223e69
arthurpetroli <arthurpetroli@gmail.com>
2022-10-26 12:31:06 -0300

    adicionar arquivos

--- ED1/arvoreavl/aula.c
+++ ED1/arvoreavl/aula.c
@@ -8,62 +8,62 @@ typedef struct  noArvore
 {
     int chave;
     struct  noArvore* dir;    
-    struct  noArvore* esq;
-    int altura;  
+    struct  noArvore* esq;  
+    int alt;
 }NoArvore;
 
 
 
 
 NoArvore* inicializaArvore(){
-    return NULL;
+    return NULL;printf("\n\n-----------------INICIO------------------\n\n");
+    imprimePreOrdem(&raiz); getchar();
+    printf("\n\n-----------------FIM------------------\n\n");
 }
 
 bool estaVazia(NoArvore** no){
     return((*no)==NULL);
 }
 
-int maxAlturaFilhos(NoArvore **no){
-    if ((*no)->esq==NULL && (*no)->dir==NULL){
-        return -1;
-    }
-    else if ((*no)->esq==NULL){
-        return (*no)->dir->altura;
-    }
-    else if ((*no)->dir==NULL){
-        return (*no)->esq->altura;
-    }  
-    else{
-        return ((*no)->dir->altura > (*no)->esq->altura) ? (*no)->dir->altura : (*no)->esq->altura;
-    }
-    
+int maxAltFilho(NoArvore **no){
+    if((*no)->esq == NULL && (*no)->dir == NULL) return (-1);
+    else if((*no)->esq == NULL) return ((*no)->dir->alt);
+    else if((*no)->dir == NULL) return ((*no)->esq->alt);
+    return((*no)->dir->alt > (*no)->esq->alt ? (*no)->dir->alt : (*no)->esq->alt);
 }
 
-int altura(NoArvore **no){
-    if ((*no)==NULL){
-        return -1;
-    }
-    else{
-        return (*no)->altura;
-    }
-}
-
-void rotacaoEsquerda(NoArvore **no){
-    NoArvore *aux;
+void rotacaoEsq(NoArvore **no){
+    NoArvore *aux=NULL;
     aux = (*no)->dir;
     (*no)->dir = aux->esq;
     aux->esq = (*no);
-    (*no)->altura = maxAlturaFilhos(no)+1;
-    aux->altura = maxAlturaFilhos(&aux)+1;
-    (*no) = aux;
+    (*no)=aux;
+    (*no)->esq->alt = maxAltFilho(&(*no)->esq) + 1;
+   // (*no)->dir->alt = maxAltFilho(&(*no)->dir) + 1;
+    (*no)->alt = maxAltFilho(no) + 1;
+}
+
+void rotacaoDir(NoArvore **no){
+    NoArvore *aux = (*no)->esq;
+    (*no)->esq=aux->dir;
+    aux->dir=(*no);
+    (*no)=aux;
+   // (*no)->esq->alt = maxAltFilho(&(*no)->esq) + 1;
+    (*no)->dir->alt = maxAltFilho(&(*no)->dir) + 1;
+    (*no)->alt = maxAltFilho(no) + 1;
+}
+
+int altura(NoArvore **no){
+    if((*no)==NULL) return -1;
+    return ((*no)->alt);
 }
 
 void insereArvore(NoArvore **no, int chave){
-    if((*no)==NULL){
+   if((*no)==NULL){
         (*no)=(NoArvore*) malloc(sizeof(NoArvore));
         (*no)->chave=chave;
         (*no)->dir=(*no)->esq=NULL;
-        (*no)->altura=0;
+        (*no)->alt=0;
         return;
     }
    if((*no)->chave == chave){
@@ -71,30 +71,36 @@ void insereArvore(NoArvore **no, int chave){
         return;
     }
     (*no)->chave > chave ? insereArvore(&((*no)->esq),chave): insereArvore(&((*no)->dir), chave);
-    (*no)->altura = maxAlturaFilhos(no) + 1;
-    if ((*no)->dir->altura - (*no)->esq->altura == 2){
-        if ((*no)->dir->dir->altura - (*no)->esq->altura == 1){
-            rotacaoEsquerda(no);
-        }
-        else{
-            rotacaoDuplaEsquerda(no);
-        }
+    (*no)->alt= maxAltFilho(no) + 1;
+    
+    if(altura(&(*no)->dir) - altura(&(*no)->esq) == 2){
+        if(altura(&(*no)->dir->dir) - altura(&(*no)->dir->esq) < 0)
+        {
+            rotacaoDir(&(*no)->dir);
+        }  
+        rotacaoEsq(no);
     }
-    else if ((*no)->esq->altura - (*no)->dir->altura == -2){
-        if ((*no)->esq->esq->altura - (*no)->dir->altura == 1){
-            rotacaoDireita(no);
-        }
-        else{
-            rotacaoDuplaDireita(no);
-        }
+    else if(altura(&(*no)->dir) - altura(&(*no)->esq) == -2){
+        if(altura(&(*no)->esq->dir) - altura(&(*no)->esq->esq) > 0)
+        {
+            rotacaoEsq(&(*no)->esq);
+        } 
+        rotacaoDir(no);
     }
 }
 
 void imprimeEmOrdem(NoArvore **no){
     if(*no==NULL) return;
     imprimeEmOrdem(&((*no)->esq));
-    printf("\n--> %d [h=%d] ", (*no)->chave, (*no)->altura);
+    printf("\n--> %d [h=%d]", (*no)->chave, (*no)->alt);
     imprimeEmOrdem(&((*no)->dir));
+}
+
+void imprimePreOrdem(NoArvore **no){
+    if(*no==NULL) return;
+    printf("\n--> %d [h=%d]", (*no)->chave, (*no)->alt);
+    imprimePreOrdem(&((*no)->esq));    
+    imprimePreOrdem(&((*no)->dir));
 }
 
 void destroiArvore(NoArvore **no){
@@ -105,178 +111,113 @@ void destroiArvore(NoArvore **no){
     (*no)=NULL;
 }
 
-NoArvore* maiorNoSubArvEsq(NoArvore **no){
-    if((*no)->dir==NULL){ 
-        NoArvore* aux = (*no);
-        if((*no)->esq!=NULL){
-            *no = (*no)->esq;
-            return (aux);
-        }else{
+void excluiElem(NoArvore **no, int chave){
+    if(*no==NULL){
+        printf("\n\n------------\n\n\t Chave nao encontrada \n\n----------------\n");
+        return;
+    }
+    if((*no)->chave==chave){
+        if((*no)->esq==NULL && (*no)->dir==NULL){
+            free(*no);
             *no=NULL;
-            return (aux);
         }
-        
-        return (aux);
-    }    
-    return maiorNoSubArvEsq(&(*no)->dir);
-}
-
-NoArvore *menorNoSubArvDir(NoArvore **no){
-    if((*no)->esq==NULL){
-        NoArvore *aux=*no;
-        if((*no)->dir!=NULL){
+        else if((*no)->esq==NULL){
+            NoArvore *aux=*no;
             *no=(*no)->dir;
+            free(aux);
         }
-        else{
-           *no=NULL;
-           return aux; 
-        } 
-        return aux;
-    }
-    return menorNoSubArvDir(&(*no)->esq);
-}
-
-void excluiElemDir(NoArvore **no, int chave){
-    if(*no==NULL){
-        printf("\n\n------------\n\n\t Chave nao encontrada \n\n----------------\n");
+        else if((*no)->dir==NULL){
+            NoArvore *aux=*no;
+            *no=(*no)->esq;
+            free(aux);
+        }
+        else{ //arrumar
+           NoArvore *aux=(*no);
+           NoArvore *anterior=(*no);
+           *no=(*no)->esq;
+           while((*no)->dir!=NULL){
+                anterior=(*no); 
+                *no=(*no)->dir;                
+           }
+           (*no)->dir=aux->dir;
+           anterior->dir=(*no)->esq;
+           if(aux!=anterior) (*no)->esq=aux->esq;
+           free(aux);           
+        }
         return;
     }
-    if((*no)->chave == chave){
-        if((*no)->esq== NULL && (*no)->dir==NULL){
-           free(*no);
-           (*no)=NULL; 
-           return;
-        }
-        if((*no)->esq== NULL){
-            NoArvore *remove=*no;
-            *no= (*no)->dir; 
-            free(remove);
-            return;
-        }
-        if((*no)->dir== NULL){
-            NoArvore *remove=*no;
-            *no= (*no)->esq; 
-            free(remove);
-            return;
-        }
-        //NoArvore *elem = maiorNoSubArvEsq(&(*no)->esq); ou
-        NoArvore *elem = maiorNoSubArvEsq(&(*no)->dir);    
-        NoArvore *remove=*no;
-        elem->dir=(*no)->dir;
-        elem->esq=(*no)->esq;
-        *no= elem; 
-        free(remove);
-        return;   
-
+    if((*no)->chave > chave) excluiElem(&(*no)->esq, chave);
+    else excluiElem(&(*no)->dir, chave);
+    (*no)->alt = maxAltFilho(no) + 1;
+     if(altura(&(*no)->dir) - altura(&(*no)->esq) == 2){
+        if(altura(&(*no)->dir->dir) - altura(&(*no)->dir->esq) < 0)  rotacaoDir(&(*no)->dir);
+        rotacaoEsq(no);
     }
-    if((*no)->chave > chave){
-        excluiElemDir(&(*no)->esq, chave);
+    else if(altura(&(*no)->dir) - altura(&(*no)->esq) == -2){
+         if(altura(&(*no)->esq->dir) - altura(&(*no)->esq->esq) > 0)  rotacaoEsq(&(*no)->esq);
+        rotacaoDir(no);
     }
-    
-    else excluiElemDir(&(*no)->dir, chave);
-}
-
-void excluiElemEsq(NoArvore **no, int chave){
-    if(*no==NULL){
-        printf("\n\n------------\n\n\t Chave nao encontrada \n\n----------------\n");
-        return;
-    }
-    if((*no)->chave == chave){
-        if((*no)->esq== NULL && (*no)->dir==NULL){
-           free(*no);
-           (*no)=NULL; 
-           return;
-        }
-        if((*no)->esq== NULL){
-            NoArvore *remove=*no;
-            *no= (*no)->dir; 
-            free(remove);
-            return;
-        }
-        if((*no)->dir== NULL){
-            NoArvore *remove=*no;
-            *no= (*no)->esq; 
-            free(remove);
-            return;
-        }
-        //NoArvore *elem = maiorNoSubArvEsq(&(*no)->esq); ou
-        NoArvore *elem = menorNoSubArvDir(&(*no)->esq);    
-        NoArvore *remove=*no;
-        elem->dir=(*no)->dir;
-        elem->esq=(*no)->esq;
-        *no= elem; 
-        free(remove);
-        return;   
-
-    }
-    if((*no)->chave > chave){
-        excluiElemDir(&(*no)->esq, chave);
-    }
-    
-    else excluiElemDir(&(*no)->dir, chave);
-}
-
-NoArvore* buscarElem(NoArvore **no, int chave){
-    if (*no == NULL)
-    {
-        return NULL;
-    }
-    if ((*no)->chave == chave)
-    {
-        return *no;
-    }
-    if ((*no)->chave > chave)
-    {
-        return buscarElem(&(*no)->esq, chave);
-    }else{
-        return buscarElem(&(*no)->dir, chave);
-    }
-      
 }
 
 int main(void){
 
     NoArvore *raiz = inicializaArvore();
 
-    insereArvore(&raiz, 10);
-
-    
-   
+    insereArvore(&raiz, 13);  
+    insereArvore(&raiz, 14);
     insereArvore(&raiz, 15);
-    insereArvore(&raiz, 13);
-    insereArvore(&raiz, 16);
-    insereArvore(&raiz, 12);
-    insereArvore(&raiz, 14); 
-
-    insereArvore(&raiz, 5);
-    insereArvore(&raiz, 3);
-    insereArvore(&raiz, 7);
-    insereArvore(&raiz, 6);
-
-    insereArvore(&raiz, 2);
-    insereArvore(&raiz, 20);
 
     printf("\n\n-----------------INICIO------------------\n\n");
-    imprimeEmOrdem(&raiz);
+    imprimePreOrdem(&raiz); getchar();
     printf("\n\n-----------------FIM------------------\n\n");
 
-    excluiElemDir(&raiz, 10);
+    insereArvore(&raiz, 12);
+    insereArvore(&raiz, 11);
+    insereArvore(&raiz, 17);
 
-    if(buscarElem(&raiz, 5)!= NULL){
-        printf("Esta presente");
-    }else{
-        printf("Nao esta presente");
-    }
+    printf("\n\n-----------------INICIO------------------\n\n");
+    imprimePreOrdem(&raiz); getchar();
+    printf("\n\n-----------------FIM------------------\n\n");
 
-    printf("\n\n-----------------Inicio------------------\n\n");
-    imprimeEmOrdem(&raiz);
-    printf("\n\n-----------------Fim------------------\n\n");
 
-    excluiElemEsq(&raiz, 7);
-        
-    printf("\n\n-----------------Inicio------------------\n\n");
-    imprimeEmOrdem(&raiz);
-    printf("\n\n-----------------Fim------------------\n\n");
+    insereArvore(&raiz, 16); 
+    insereArvore(&raiz, 8);
+    insereArvore(&raiz, 9);
+    
+    printf("\n\n-----------------INICIO------------------\n\n");
+    imprimePreOrdem(&raiz); getchar();
+    printf("\n\n-----------------FIM------------------\n\n");
 
+
+    excluiElem(&raiz, 8);
+
+    printf("\n\n-----------------INICIO------------------\n\n");
+    imprimePreOrdem(&raiz); getchar();
+    printf("\n\n-----------------FIM------------------\n\n");
+
+    excluiElem(&raiz, 13);  
+    excluiElem(&raiz, 14);
+    excluiElem(&raiz, 15);
+    excluiElem(&raiz, 12);
+
+    printf("\n\n-----------------INICIO------------------\n\n");
+    imprimePreOrdem(&raiz); getchar();
+    printf("\n\n-----------------FIM------------------\n\n");
+
+    excluiElem(&raiz, 11);
+    excluiElem(&raiz, 17);
+    excluiElem(&raiz, 16); 
+    excluiElem(&raiz, 9);
+    
+    printf("\n\n-----------------INICIO------------------\n\n");
+    imprimePreOrdem(&raiz); getchar();
+    printf("\n\n-----------------FIM------------------\n\n");
+
+    excluiElem(&raiz, 777);
+
+    printf("\n\n-----------------INICIO------------------\n\n");
+    imprimePreOrdem(&raiz); getchar();
+    printf("\n\n-----------------FIM------------------\n\n");
+    
     destroiArvore(&raiz);
 }
