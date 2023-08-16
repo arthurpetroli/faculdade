cddbd839e80e5c6a2c927cee36a2ade6419533ea
Arthur Petroli <87681962+arthurpetroli@users.noreply.github.com>
2023-08-16 14:36:39 -0300

    Create MainEstudantes.java

---
+++ Poo/MainEstudantes.java
@@ -0,0 +1,15 @@
+public class Main {	
+
+/*
+   TODO: Crie 2 (dois) objetos Estudantes
+   
+   TODO: Imprima cada atributo do objeto com metodos acessores.
+*/
+
+	public static void main(String[] args) {
+		Estudante rosa = new Estudante("Rosa","Rua 123","456","Silva"); //<-- argumentos
+		
+		System.out.println(rosa);
+	}
+
+}
