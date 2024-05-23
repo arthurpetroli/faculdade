20f50367f25e698f86bb586e1086ff1ddbbe1557
arthurpetroli <arthurpetroli@gmail.com>
2024-05-23 08:59:12 -0300

    adicionar arquivos

--- FDC/Prova 3/aula12.m
+++ FDC/Prova 3/aula12.m
@@ -1,8 +1,36 @@
 syms s
 
 kp = limit(500*(s+2)*(s+5)/((s+8)*(s+10)*(s+12)), s, 0)
-kv = s*limit(500*(s+2)*(s+5)/((s+8)*(s+10)*(s+12)), s, 0)
-ka = (s^2)*limit(500*(s+2)*(s+5)/((s+8)*(s+10)*(s+12)) , s, 0)
+kv = limit((s)*500*(s+2)*(s+5)/((s+8)*(s+10)*(s+12)), s, 0)
+ka = limit((s^2)*500*(s+2)*(s+5)/((s+8)*(s+10)*(s+12)) , s, 0)
+estep = 1 / (1 + kp)
+eramp = 1 / kv
+eparabol = 1 / ka
+
+
+%%
+syms s
+kp = limit(1000*(s+8)/((s+7)*(s+9)), s, 0)
+kv = limit((s)*1000*(s+8)/((s+7)*(s+9)), s, 0)
+ka = limit((s^2)*1000*(s+8)/((s+7)*(s+9)) , s, 0)
+estep = 1 / (1 + kp)
+eramp = 1 / kv
+eparabol = 1 / ka
+
+%%
+syms s
+kp = limit(1000*(s+12)/((s+14)*(s+18)), s, 0)
+kv = limit((s)*1000*(s+12)/((s+14)*(s+18)), s, 0)
+ka = limit((s^2)*1000*(s+12)/((s+14)*(s+18)) , s, 0)
+estep = 1 / (1 + kp)
+eramp = 1 / kv
+eparabol = 1 / ka
+
+%%
+syms s
+kp = limit(1000*(s+5)/(s*(s+6)*(s+7)*(s+8)), s, 0)
+kv = limit((s)*1000*(s+5)/(s*(s+6)*(s+7)*(s+8)), s, 0)
+ka = limit((s^2)*1000*(s+5)/(s*(s+6)*(s+7)*(s+8)) , s, 0)
 estep = 1 / (1 + kp)
 eramp = 1 / kv
 eparabol = 1 / ka
