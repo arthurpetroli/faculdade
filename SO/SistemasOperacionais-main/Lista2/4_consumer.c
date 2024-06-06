458cfd32ddd5296b8de08bf5523e1d64af69eee7
Arthur Petroli <87681962+arthurpetroli@users.noreply.github.com>
2024-06-06 09:54:47 -0300

    Update 4_consumer.c

--- SO/SistemasOperacionais-main/Lista2/4_consumer.c
+++ SO/SistemasOperacionais-main/Lista2/4_consumer.c
@@ -6,114 +6,71 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/wait.h>
-#include <pthread.h>
 
 #define SHM_SIZE 1024
-const int REP = 5;
-char dado;
-key_t key;
-int shmid, flagsid;
-char *data;
-int *flags;
-pthread_mutex_t mutex;
-pthread_cond_t cond;
+#define N 5
 
-void *producer(void *arg)
-{
+int shmid, shmid2;
+int *counter;
+char *buf;
+
+int in = 0;
+int out = 0;
+
+void producer() {
     printf("Producer was born!\n");
 
-    for (int i = 0; i < REP; i++)
-    {
-        pthread_mutex_lock(&mutex);
-
-        while (flags[1] && (flags[3] == 1))
-        {
-            pthread_cond_wait(&cond, &mutex);
-        }
-
-        data[i] = (char)i + 0x61;
-        printf("Stored... %c \n", data[i]);
-
-        flags[3] = 1;
-
-        flags[0] = 0;
-
-        pthread_cond_signal(&cond);
-        pthread_mutex_unlock(&mutex);
+    while (1) {
+        while (*counter == N) ;
+        
+        buf[in] = (char) in + 0x61;
+        printf("Stored... %c \n", buf[in]);
+        
+        in = ++in % N;
+        (*counter)++;
     }
-
-    return NULL;
 }
 
-void *consumer(void *arg)
-{
+void consumer() {
     printf("Consumer was born!\n");
 
-    for (int i = 0; i < REP; i++)
-    {
-        pthread_mutex_lock(&mutex);
-
-        while (flags[0] && (flags[3] == 0))
-        {
-            pthread_cond_wait(&cond, &mutex);
-        }
-
-        dado = data[i];
-
-        data[i] = ' ';
-
+    while (1) {
+        while (*counter == 0) ;
+        
+        char dado = buf[out];
         printf("Consumed... %c \n", dado);
-
-        flags[3] = 0;
-
-        flags[1] = 0;
-
-        pthread_cond_signal(&cond);
-        pthread_mutex_unlock(&mutex);
+        
+        out = ++out % N;
+        (*counter)--;
     }
-
-    return NULL;
 }
 
-int main()
-{
+int main() {
     printf("The Producer x Consumer Problem\n");
-    int status;
 
-    key = ftok("/home", 'A');                      //  Segmento
-    shmid = shmget(key, 1024, 0644 | IPC_CREAT);   //
-    data = (malloc(5 * sizeof(char)));             //     1
-    data = shmat(shmid, (void *)0, 0);             //
+    key_t key = ftok("/home", 'A');
+    shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT);
+    counter = shmat(shmid, (void *)0, 0);
 
-    // Peterson
-    key = ftok("/home/downloads", 'B');            //  Segmento
-    flagsid = shmget(key, 1024, 0644 | IPC_CREAT); //
-    flags = (malloc(3 * sizeof(int)));             //     2
-    flags = shmat(flagsid, (void *)0, 0);          //
+    key_t key2 = ftok("/home/downloads", 'B');
+    shmid2 = shmget(key2, SHM_SIZE, 0644 | IPC_CREAT);
+    buf = shmat(shmid2, (void *)0, 0);
 
-    flags[0] = 1;
-    flags[1] = 0;
-    flags[3] = 0;
+    *counter = 0;
 
-    pthread_t producerThread, consumerThread;
+    int pid = fork();
 
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cond, NULL);
+    if(pid == 0){
+        producer();       
+    } else {
+        consumer();
+    }
 
-    pthread_create(&producerThread, NULL, producer, NULL);
-    pthread_create(&consumerThread, NULL, consumer, NULL);
+    shmdt(counter);
+    shmctl(shmid, IPC_RMID, NULL);
 
-    pthread_join(producerThread, NULL);
-    pthread_join(consumerThread, NULL);
-
-    pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond);
-
-    shmdt(data);                              //segmento 3
-    shmctl(shmid, IPC_RMID, NULL);            //
-
-    shmdt(flags);                             //segmento 4
-    shmctl(flagsid, IPC_RMID, NULL);          //
+    shmdt(buf);
+    shmctl(shmid2, IPC_RMID, NULL);
 
     return 0;
 }
