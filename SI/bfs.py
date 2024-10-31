9b4c9c9123ae9c5eabe58ff1c5976fe7cac2d725
arthurpetroli <arthurpetroli@gmail.com>
2024-10-31 11:14:55 -0300

    adicionar arquivos

--- SI/bfs.py
+++ SI/bfs.py
@@ -23,6 +23,13 @@ class Graph:
 
 
 def solve(grafo, start, goal):
+    
+    def find_zero(self): 
+        for i in range(len(self.matriz)): 
+            for j in range(len(self.matriz[i])): 
+                if self.matriz[i][j] == 0: 
+                    return i, j
+    
     stack = []
     visited = []
     visited.append(start)
@@ -36,37 +43,20 @@ def solve(grafo, start, goal):
                 if result is not None:
                     stack.append(neighbour)
                     return result
-        return stack
-    backtracking(grafo, start, start, goal)
-    stack.append(start)
-
-    return stack
-
-
-
 grafo = Graph({})
-# start = 2
-# goal = 6
-# grafo.addNode(2, [3, 4])
-# grafo.addNode(3, [5, 8])
-# grafo.addNode(4, [6, 3])
-# grafo.addNode(5, [2, 6])
-# grafo.addNode(6, [])
-# print(solve(grafo, start, 0))
-# start = "A"
-# goal = "I"
-# grafo.addNode("A", ["B", "C", "D"])
-# grafo.addNode("B", ["E", "F"])
-# grafo.addNode("E", ["H", "I"])
-# grafo.addN['A',ode("H", [])
-# grafo.addNode("I", [])
-# grafo.showGraph()
-start = "A"
-goal = "C"
+
+start = [[2,8,3],
+        [1,6,4],
+        [7,0,5]]
+
+goal = [[1,2,3],
+        [8,0,4],
+        [7,6,5]]
+
 grafo.addNode("A", ["E", "B", "D", "C"])
 grafo.addNode("B", ["E", "D", "A", "C"])
 grafo.addNode("E", ["B", "C", "D", "A"])
 grafo.addNode("D", ["E", "B", "A", "C"])
 grafo.addNode("C", ["E", "B", "D", "A"])
 grafo.showGraph()
-print(solve(grafo, start, ""))
+print(solve(grafo, start, goal))
